9bc6df454e8251c0c97eccb5a9335171999b4cb7
vaishnavanm5 <vaishnavanm5@gmail.com>
2023-02-19 14:41:17 +0530

    db connection creation

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 /* eslint-disable no-undef */
 const express = require("express")
+const { db } = require("./db")
+require('dotenv').config()
+
 const app = express()
 const PORT = process.env.PORT || 5000
 
 app.listen((PORT), () => {
     console.log(`server up and running on port ${PORT}`)
 })
+
+db();
